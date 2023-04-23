0c40e3a2fa3aa6cd6dc4cbe6ff6e6101b8098544
Luke <121516961+Afrozez@users.noreply.github.com>
2023-04-23 09:42:24 -0700

    selected the header and final scoreboard and applied the hide class

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ quizzzBox = document.querySelector('.quiz-box');
 mainBox = document.getElementById("main-box");
 yourFinalScore = document.getElementById("your-final-score");
 displayScore = document.querySelector('.display-score');
+header = document.querySelector('header');
+
 let timer;
 let timeLeft = 60;
 let score = 0;
@@ -11,9 +13,13 @@ let finalScore = `<p>Your final score is ${score}<p>`;
 
 
 
+submitScoreButton= document.getElementById("submit-score");
+submitScoreButton.addEventListener("click", displayHighscores);
 
-
-  
+function displayHighscores() {
+        yourFinalScore.className = 'hide';
+        header.className = 'hide';
+}
 
 
 startButton.addEventListener("click", hideMainBox);
