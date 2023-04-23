f811b878e7948ae31a507a76acfe8d85f6bb34e4
Luke <121516961+Afrozez@users.noreply.github.com>
2023-04-22 19:40:30 -0700

    worked on the timer and applied the css

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,41 @@
+startButton = document.getElementById('start-button');
 const countdown = document.getElementById('countdown');
+quizzzBox = document.querySelector('.quiz-box');
+mainBox = document.getElementById("main-box");
+let timer;
 let timeLeft = 60;
+currentScore = "";
+yourFinalScore = "<h1>All Done!</h1><p> Your final score is.</p>";
 
-setInterval(() => {
+
+startButton.addEventListener("click", function() {
+    
+    timer = setInterval(() => {
     timeLeft--;
     countdown.textContent = timeLeft;
-}, 1000);
+
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            countdown.textContent = 'Time is up!';
+            mainBox.innerHTML = yourFinalScore + currentScore;
+            
+        }
+ }, 1000);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
