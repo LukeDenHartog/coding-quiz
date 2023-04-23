ec2d8df4bed35e82a74d860494b9ba6e0a021bce
Luke <121516961+Afrozez@users.noreply.github.com>
2023-04-22 22:32:13 -0700

    added a All done! final score thing

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,19 @@ startButton = document.getElementById('start-button');
 const countdown = document.getElementById('countdown');
 quizzzBox = document.querySelector('.quiz-box');
 mainBox = document.getElementById("main-box");
+yourFinalScore = document.getElementById("your-final-score");
 let timer;
 let timeLeft = 60;
 currentScore = "";
-yourFinalScore = "<h1>All Done!</h1><p> Your final score is.</p>";
+finalScoreHeader = "<h1>All Done!</h1>";
 
 
+
+
+  
+
+
+startButton.addEventListener("click", hideMainBox);
 startButton.addEventListener("click", function() {
     
     timer = setInterval(() => {
@@ -15,18 +22,18 @@ startButton.addEventListener("click", function() {
     countdown.textContent = timeLeft;
 
         if (timeLeft <= 0) {
+            yourFinalScore.className = 'show';
             clearInterval(timer);
-            countdown.textContent = 'Time is up!';
-            mainBox.innerHTML = yourFinalScore + currentScore;
+            countdown.textContent = ' Time is up!';
+            mainBox.innerHTML = finalScoreHeader + "Your final score is" + currentScore;
             
         }
  }, 1000);
 });
 
-
-
-
-
+function hideMainBox() {
+    mainBox.style.display = "none";
+  }
 
 
 
